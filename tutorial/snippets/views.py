2890c036d8b70fce33f46a100abc6e230ddb3704
CQ <chihqing98@hotmail.com>
2019-05-07 12:44:52 +0800

    Link snippets with user, update serializers and add permission

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,6 +3,7 @@ from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
+from rest_framework import permissions
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -16,8 +17,14 @@ class UserDetail(generics.RetrieveAPIView):
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
